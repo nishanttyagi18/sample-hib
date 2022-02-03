21426daa5dc769c5ac54e25b66d58c26e428006a
Nishant Tyagi <ntyagi893@gmail.com>
2022-02-03 19:01:18 +0530

    One to Many with New table and Reference in Laptop table.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -29,14 +29,17 @@ public class App {
         student.setRollno(1);
         student.setName("Nishant Tyagi");
         student.setMarks(100);
-        student.getLaptop().add(laptop);   // one to many mapping
 
+        laptop.setStudent(student);
+        student.getLaptop().add(laptop);
+
+        /**
+         *  Reference is created in Laptop table
+         *  But it can't be created in Student because student has roll no as primary key
+         */
 
         session.save(student);
         session.save(laptop);
-
-
-
 
         tx.commit();
         session.close();
--- src/main/java/org/example/Laptop.java
+++ src/main/java/org/example/Laptop.java
@@ -3,12 +3,26 @@ package org.example;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Laptop {
     @Id
     private int id;
     private String lname;
+
+    @ManyToOne
+    private Student student;
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+
 
     public int getId() {
         return id;
